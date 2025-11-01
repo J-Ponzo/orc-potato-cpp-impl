e30bda0aa8639feda2ce39a178b1c6cb130051c6
jponzo <jeremy.ponzo@gmail.com>
2025-11-01 08:39:30 +0100

    refact so that cpp impl can be symlinked into ORC plugin (solving .dll dependency issue)

---
+++ src/register_types.cpp
@@ -0,0 +1,30 @@
+#include "register_types.h"
+
+#include "public/potato_cpp_renderer.h"
+#include "public/potato_cpp_scene_proxy.h"
+
+#include <impl_registry.h>
+#include <gdextension_interface.h>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+using namespace godot;
+
+void initialize_potato_cpp_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+
+	GDREGISTER_RUNTIME_CLASS(ORC_PotatoCPPRenderer);
+	GDREGISTER_RUNTIME_CLASS(ORC_PotatoCPPSceneProxy);
+
+	UtilityFunctions::print("[ORC] Potato C++ implementation registered");	// TODO move this in ORC_ImplRegistry
+}
+
+void uninitialize_potato_cpp_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+}
+
+ORC_REGISTER_IMPL_MODULE("potato-cpp", initialize_potato_cpp_module, uninitialize_potato_cpp_module);
---
+++ src/register_types.h
@@ -0,0 +1,11 @@
+#ifndef POTATO_CPP_REGISTER_TYPES_H
+#define POTATO_CPP_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_potato_cpp_module(ModuleInitializationLevel p_level);
+void uninitialize_potato_cpp_module(ModuleInitializationLevel p_level);
+
+#endif // POTATO_CPP_REGISTER_TYPES_H
